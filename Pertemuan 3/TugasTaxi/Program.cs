c1aed668367480b205ea0b898a9f74150622e3ac
HardyWisnu <hardywisnu@amikom.students.ac.id>
2022-03-24 19:03:13 +0700

    initial Tugas 3

---
+++ Pertemuan 3/TugasTaxi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.onDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            Console.WriteLine();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pertemuan 3/TugasTaxi/taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    public class taxi
+    {
+        public String DriverName { get; set; }
+        public bool onDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            String duty;
+            if (onDuty)
+            {
+                duty = "Yess";
+            }
+            else
+            {
+                duty = "No";
+            }
+
+            Console.WriteLine("Driver Name\t\t: {0}", DriverName);
+            Console.WriteLine("Status OnDuty\t\t: {0}", duty);
+            Console.WriteLine("Number of Passenger\t: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} In Order", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Out of Order", DriverName);
+        }
+    }
+}
